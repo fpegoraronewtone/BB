9115849a70d3250b1a71d5adfc9b3198e169a996
mpeg <mpeg@blackbeardcrew.com>
2013-02-12 17:04:32 +0100

    bbTime() and some improvements to extend()

--- basics.php
+++ basics.php
@@ -8,6 +8,14 @@
 
 
 /**
+ * Start time used to debug execution time
+ */
+if (!defined('BB_START')) {
+	define('BB_START', microtime());
+}
+
+
+/**
  * debug() then die.
  */
 if (!function_exists('ddebug')) {    
@@ -18,6 +26,71 @@ if (!function_exists('ddebug')) {
         }
     }
 }
+
+
+/**
+ * Return elapsed time from BB initialization.
+ */
+function bbTime( $print = true, $die = true ) {
+	$time 	= round(microtime() - BB_START, 4);
+	$stime	= $time . 's.';
+	if ($print === 2 && Configure::read('debug')) {
+		debug($stime); 
+	} elseif ($print) {
+		echo $stime;
+	}
+	if ($die) {
+		die();
+	}
+	return $time;
+}
+
+/**
+ * Dump a stack of timer results into a log file in TMP folder
+ * to elaborate average time of given breakpoint.
+ */
+function bbAvgTime($print = true, $die = true) {
+	$time = bbTime(false,false);
+	$_logPath = TMP . 'bbAvgTime.txt';
+	
+	if (file_exists($_logPath)) {
+		$log = unserialize(file_get_contents($_logPath));
+		if (!is_array($log)) unset($log);
+	}
+	
+	if (empty($log)) {
+		$log = array(
+			'sum' 	=> 0,
+			'tot'	=> 0,
+			'avg'	=> 0,
+			'time' 	=> array(),
+		);
+	}
+	
+	$log['time'][] = $time;
+	$log['sum'] += $time;
+	$log['tot'] += 1;
+	$log['avg'] = round($log['sum'] / $log['tot'], 4);
+	
+	@file_put_contents($_logPath, serialize($log));
+	
+	if ($print === 3 && Configure::read('debug')) { debug($log); }
+	elseif ($print === 3) { print_r($log); }
+	elseif ($print === 2 && Configure::read('debug')) { debug($log['avg'].' s.'); }
+	elseif ($print) { echo $log['avg'].' s.'; }
+	
+	if ($die) die();
+	return $log;
+}
+
+function bbAvgReset() {
+	@unlink( TMP . 'bbAvgTime.txt' );	
+}
+
+
+
+
+
 
 
 // CakePHP hard dependencies:
--- plugins.php
+++ plugins.php
@@ -1,27 +1,18 @@
 <?php
+
 /**
-	* BB - BlackBeard Core Plugin
-	* 
-	* plugins auto loading layer
-	* ============================
-	* 
-	* BB works as plugins loader utility.
-	* BB scan your plugin folders to include all available plugins into the app.
-	* If a plugin exists into plugin's repositories it will be included.
-	* 
-	* 
-	* 
-	*/
-
-
-
-
-
-
-
-
-
-
+ * BB - BlackBeard Core Plugin
+ * 
+ * plugins auto loading layer
+ * ============================
+ * 
+ * BB works as plugins loader utility.
+ * BB scan your plugin folders to include all available plugins into the app.
+ * If a plugin exists into plugin's repositories it will be included.
+ * 
+ * 
+ * 
+ */
 /**
  * Scan plugins repositories and compose a list of plugins
  * to be loaded into the app.
@@ -29,53 +20,52 @@
  * This operation is cached to improve application performance!
  * 
  */
-
 // @TODO: this piece of logic should be cached when debug off!
 #@$plugins = read_from_cache_layer();
 
 if (empty($plugins)) {
-				foreach	(App::path('plugins')	as	$path)	{
-								$repo	=	new	Folder($path);
-								foreach	($repo->read()[0]	as	$pluginName)	{
+	foreach (App::path('plugins') as $path) {
+		$repo = new Folder($path);
+		foreach ($repo->read()[0] as $pluginName) {
 
-												// skip plugins that are already loaded 
-												if	(CakePlugin::loaded($pluginName)) {
-																continue;
-												}
+			// skip plugins that are already loaded 
+			if (CakePlugin::loaded($pluginName)) {
+				continue;
+			}
 
-												$plugin	=	array(
-																'name' => $pluginName,
-																'base'	=>	$path,
-																'path'	=>	$path	.	$pluginName	.	DS,
-																'order' => 5000,
-																'config'	=>	array(
-																				'bootstrap'	=>	false,
-																				'routes'	=>	false
-																)
-												);
+			$plugin = array(
+				'name' => $pluginName,
+				'base' => $path,
+				'path' => $path . $pluginName . DS,
+				'order' => 5000,
+				'config' => array(
+					'bootstrap' => false,
+					'routes' => false
+				)
+			);
 
-												if	(file_exists($plugin['path']	.	'Config/bootstrap.php'))	{
-																$plugin['config']['bootstrap']	=	true;
-												}
+			if (file_exists($plugin['path'] . 'Config/bootstrap.php')) {
+				$plugin['config']['bootstrap'] = true;
+			}
 
-												if	(file_exists($plugin['path']	.	'Config/routes.php'))	{
-																$plugin['config']['routes']	=	true;
-												}
+			if (file_exists($plugin['path'] . 'Config/routes.php')) {
+				$plugin['config']['routes'] = true;
+			}
 
-												if	(file_exists($plugin['path']	.	'bb_order'))	{
-																$plugin['order'] = file_get_contents($plugin['path']	.	'bb_order');
-												}
+			if (file_exists($plugin['path'] . 'bb_order')) {
+				$plugin['order'] = file_get_contents($plugin['path'] . 'bb_order');
+			}
 
 
-												$plugins[] = $plugin;
-								}
-				}
+			$plugins[] = $plugin;
+		}
+	}
 
-				// Apply sort based on configured flag
-				$plugins = sortByKey($plugins, 'order');
-				
-				// @TODO: write to a cache layer
-				#write_to_cache_layer($plugins);
+	// Apply sort based on configured flag
+	$plugins = sortByKey($plugins, 'order');
+
+	// @TODO: write to a cache layer
+	#write_to_cache_layer($plugins);
 }
 
 
@@ -89,7 +79,6 @@ if (empty($plugins)) {
  * Load Configured Plugins
  */
 foreach ($plugins as $plugin) {
-				CakePlugin::load($plugin['name'], $plugin['config']);
-				//BB::write('plugin.'.$plugin['name'], $plugin);
-				//BB::extend('plugin', array($plugin['name'] => $plugin));
+	CakePlugin::load($plugin['name'], $plugin['config']);
+	BB::extendKey('plugin', array($plugin['name'] => $plugin));
 }
