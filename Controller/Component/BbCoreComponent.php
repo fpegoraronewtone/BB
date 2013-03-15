e942051c3bd1d229fd17cc3af261af5b62ebc7ae
mpeg <mpeg@blackbeardcrew.com>
2013-03-15 12:06:02 +0100

    loadHelper, loadComponent BbCore utilities

--- Controller/Component/BbCoreComponent.php
+++ Controller/Component/BbCoreComponent.php
@@ -8,17 +8,24 @@
  */
 class BbCoreComponent extends Component {
 	
+	protected $_Collection = null;
 	protected $_Controller = null;
 	
+	public function __construct(ComponentCollection $collection, $settings = array()) {
+		$this->_Collection = $collection;
+		$this->_Controller = $collection->getController();
+	}
+	
 	public function initialize(Controller $Controller) {
-		
-		$this->_Controller = $Controller;
+		// Alias BB's components extensions
+		$this->loadComponent(array(
+			'Session' => array('className' => 'BB.BbSession')
+		), true);
 		
 		// Inject BB's helpers extensions
-		$this->_Controller->helpers = BB::extend($Controller->helpers, array(
+		$this->loadHelper(array(
 			'Html' => array('className' => 'BB.BbHtml')
 		));
-		
 	}
 	
 	
@@ -63,5 +70,70 @@ class BbCoreComponent extends Component {
 		}
 		return $default;
 	}
+	
+	
+	/**
+	 * Load Helper Utility
+	 */
+	function loadHelper($list = array()) {
+		if (!is_array($list)) {
+			$list = array($list);
+		}
+		$this->_Controller->helpers = BB::extend($this->_Controller->helpers, $list);
+	}
+	
+
+	/**
+	 * Load Component Utility
+	 */
+	public function loadComponent($list = array(), $force = false) {
+		if (!is_array($list)) {
+			$list = array($list);
+		}
+		foreach ($list as $component=>$config) {
+			if (is_int($component)) {
+				$component = $config;
+				$config = null;
+			}
+			
+			// define name and alias
+			list($plugin, $componentName) = pluginSplit($component);
+			$aliasPlugin = $plugin;
+			$aliasComponent = $component;
+			$aliasComponentName = $componentName;
+			// apply aliased className to loading settings
+			if (!empty($config['className'])) {
+				$aliasComponent = $config['className'];
+				list($aliasPlugin, $aliasComponentName) = pluginSplit($aliasComponent);
+			}
+			
+			
+			// search inside controller's loaded components for existing
+			// instance
+			if (isset($this->_Controller->{$componentName}) && !$force) {
+				continue;
+			}
+			
+			// import class and create instance of the new component
+			// it uses "alias" version!
+			App::import('Component', $aliasComponent);
+			$componentFullName = $aliasComponentName.'Component';
+			$component = new $componentFullName($this->_Collection, $config);
+
+			if (method_exists($component, 'initialize')) {
+				$component->initialize($this->_Controller);
+			}
+			if (method_exists($component, 'startup')) {
+				$component->startup($this->_Controller);
+			}
+			
+			// insert into controller's components list and return instance.
+			$this->_Controller->{$componentName} = $component;
+			return $this->_Controller->{$componentName};
+		}
+	}
+	
+	
+	
 	
 }
